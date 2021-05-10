8e022212ac52a1c10ad9934b63cef32dc2c18126
Marcandy <marcandy.nj@gmail.com>
2021-05-09 23:08:52 -0400

    album component first part

---
+++ components/Album/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+export type AlbumProps = {
+    id: String,
+    imageUri: String,
+    artistHeadline: String
+}
+
+const Album = (props: AlbumProps) => {
+    <View>
+    </View>
+}
+
+export default Album
