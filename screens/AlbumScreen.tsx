efb2cb796ca97cb4183c4cb74283d708a96d71ea
Marcandy <marcandy.nj@gmail.com>
2021-07-15 06:38:15 -0400

    album component update

--- screens/AlbumScreen.tsx
+++ screens/AlbumScreen.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { View, Text } from "react-native";
+import { useRoute } from '@react-navigation/core';
 
-const AlbumScreen = () => (
-    <View>
-        <Text>This is the Album screen</Text>
-    </View>
-)
+const album = {
+    id: '11',
+    name: 'Good vibes',
+    by: 'Spotify',
+    numberOfLikes: 38,
+    imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+    artistsHeadline: 'Taylor Swift, Kygo Objective C, Avicii',
+    songs: [{
+      id: '1',
+      imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+      title: 'High on You',
+      artist: 'Helen',
+    },{
+      id: '2',
+      imageUri: 'https://cdn6.f-cdn.com/contestentries/1485199/27006121/5ca3e39ced7f1_thumb900.jpg',
+      title: 'Lorem Ipsum',
+      artist: 'Artist 2',
+    },{
+      id: '3',
+      imageUri: 'https://images-na.ssl-images-amazon.com/images/I/61F66QURFyL.jpg',
+      title: 'Hello from the other side',
+      artist: 'Helen',
+    },{
+      id: '4',
+      imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+      title: 'High on You',
+      artist: 'Helen',
+    }]
+  };
+
+const AlbumScreen = () => {
+
+    const route = useRoute();
+
+    useEffect(()=> {
+        console.log(route)
+    }, []);
+
+    return (
+        <View>
+            <Text>This is the Album screen</Text>
+        </View>
+    )
+}
 
 export default AlbumScreen;
