041256a3636176bef7f27aec584b4a864d777c4e
Marcandy <marcandy.nj@gmail.com>
2021-07-27 19:54:44 -0400

    player widget useEffect playsong()

--- components/PlayerWidget.tsx/index.tsx
+++ components/PlayerWidget.tsx/index.tsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, Image } from 'react-native';
 import { Song } from '../../types';
 import styles from './styles';
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { Audio } from 'expo-av'
+
 
 const song = {
     id: '2',
+    uri: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
     imageUri: 'https://cdn6.f-cdn.com/contestentries/1485199/27006121/5ca3e39ced7f1_thumb900.jpg',
     title: 'Lorem Ipsum',
     artist: 'Artist 2',
 };
 
+
+
 const PlayerWidget = () => {
+    const onPlayBackStatusUpdate = (status) => {
+        console.log(status)
+    }
+  
+    const playSong = async () => {
+        const { sound } = Audio.Sound.createAsync(
+            { uri: song.uri },
+            { shouldPlay: true },
+            onPlayBackStatusUpdate,
+        )
+    }
+
+    useEffect(() => {
+        playSong()
+    }, [])
+
     return (
         <View style={styles.container}>
             <Image source={{uri: song.imageUri }} style={styles.image}/>
